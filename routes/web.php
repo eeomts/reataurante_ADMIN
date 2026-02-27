973c8743b0f67137fec4e75d66dcac39fb299ea6
eeomts <mateolimoreira7@gmail.com>
2026-02-27 18:37:49 -0300

    new routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UsersController;
 
 Route::get('/', function () {
@@ -13,7 +14,20 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
-// Users
+// Forgot password
+Route::get('/forgot-password', [AuthController::class, 'showForgotPassword'])->name('password.request');
+Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
+
+
 Route::middleware('auth')->group(function () {
-    Route::resource('users', UsersController::class);
+
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+    Route::post('/register', [UsersController::class, 'register'])->name('users.register');
+
+
+    Route::resource('users', UsersController::class)->except(['create', 'store', 'show']);
+
 });
