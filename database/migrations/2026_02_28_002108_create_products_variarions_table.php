8c928805832db42951e7c6e2769e8593d65956ec
eeomts <mateolimoreira7@gmail.com>
2026-02-28 22:06:32 -0300

    migrations produtos

---
+++ database/migrations/2026_02_28_002108_create_products_variarions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('product_variations', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('product_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('name'); // Ex: Pequeno, Médio, 2L
+            $table->decimal('price', 10, 2);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variations');
+    }
+};
