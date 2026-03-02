9243780e102cd6e15e791218da6e64f571b0d0d5
eeomts <mateolimoreira7@gmail.com>
2026-03-01 23:30:15 -0300

    Atualiza rotas para incluir prefixo 'admin' e redireciona para o dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,28 +6,26 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UsersController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
+        return redirect()->route('dashboard');
+    });
 // Auth
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
-
-// Forgot password
-Route::get('/forgot-password', [AuthController::class, 'showForgotPassword'])->name('password.request');
-Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
+Route::get('/admin/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/admin/login', [AuthController::class, 'login']);
+Route::post('/admin/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
 
-Route::middleware('auth')->group(function () {
+Route::get('/admin/forgot-password', [AuthController::class, 'showForgotPassword'])->name('password.request');
+Route::post('/admin/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
 
+Route::middleware('auth')->prefix('admin')->group(function () {
+
+    Route::get('/', function () {
+        return redirect()->route('dashboard');
+    });
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-
     Route::post('/register', [UsersController::class, 'register'])->name('users.register');
 
-
     Route::resource('users', UsersController::class)->except(['create', 'store', 'show']);
-
 });
